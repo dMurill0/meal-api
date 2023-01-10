9980dc4515d673e5c0ef5543e8d708f90a702a77
Daniel Murillo <sittingbout@gmail.com>
2023-01-10 12:27:28 +0100

    añadida redireccion de busqueda

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -2,21 +2,36 @@ import Link from "next/link";
 import React from "react";
 import { GoSearch } from "react-icons/go";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 type Props = {};
 
 function Navbar({}: Props) {
   const [search, setSearch] = useState("");
-  function handleSearch() { 
+  const router = useRouter();
+  function handleSearch() {
     let input = document.getElementById("input-srch");
     setSearch(input.value);
-    alert(search);
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length > 0) {
+          console.log(data.meals[0]);
+          let id = data.meals[0].idMeal;
+          router.push(`Search/${id}`);
+        } else {
+          alert("no hay recetas");
+        }
+      });
   }
 
   return (
     <nav>
       <ul className="list-none flex mx-6 py-2 shadow-b-lg space-x-4 md:space-x-12 lg:space-x-28 justify-center text-xl mr-28 mt-8 items-center uppercase">
-        <Link className="mx-2 hover:bg-orange-600 -mb-px mr-1 inline-block border-l border-t border-r  rounded-t py-2 px-4 " href={""}>
+        <Link
+          className="mx-2 hover:shadow-2xl hover:shadow-orange-200 -mb-px mr-1 inline-block border-l border-t border-r  rounded-t py-2 px-4 "
+          href={""}
+        >
           Menu
         </Link>
         <Link
@@ -25,7 +40,10 @@ function Navbar({}: Props) {
         >
           Foods
         </Link>
-        <Link className="mx-2 hover:bg-orange-600 -mb-px mr-1 inline-block border-l border-t border-r  rounded-t py-2 px-4 " href={""}>
+        <Link
+          className="mx-2 hover:bg-orange-600 -mb-px mr-1 inline-block border-l border-t border-r  rounded-t py-2 px-4 "
+          href={""}
+        >
           Services
         </Link>
         <Link
@@ -42,13 +60,9 @@ function Navbar({}: Props) {
           placeholder="Search a meal"
           id="input-srch"
         />
-        <Link
-          href={`/search/${search}`}
-          id="search-btn"
-          onClick={handleSearch}
-        >
+        <button id="search-btn" onClick={handleSearch}>
           <GoSearch size={30} />
-        </Link>
+        </button>
       </div>
     </nav>
   );
